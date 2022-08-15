36c67fb9d9c7250c9430b68e416c36d312f92de4
miranda0111 <75111602+miranda0111@users.noreply.github.com>
2022-08-15 21:02:29 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -3,7 +3,8 @@ const MMSLS = [
     "https://www.maimemo.com/share/page?uid=15249640&pid=d9854f92d3dbb94c2b6ac1c01be49876&tid=73efc8782196dd1bb0c9300ec4c2cf44",
     "https://www.maimemo.com/share/page?uid=24205654&pid=39b3968ca9169c65eccd24b7d376ca80&tid=d55d281129f6e85f1f67c7c7d0ea73b6",
     "https://www.maimemo.com/share/page?uid=9265100&pid=39b3968ca9169c65eccd24b7d376ca80&tid=9bc1d06ff8eae7e62eb2697da204adcc",
-  ]
+"https://www.maimemo.com/share/page?tid=90a7e415965bfe5de96a042d8331bcd3&uid=17134313&pid=d9854f92d3dbb94c2b6ac1c01be49876", 
+ ]
   /**
    * 生成随机数字
    * @param {number} min 最小值（包含）
