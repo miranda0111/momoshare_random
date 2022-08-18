96c3fcf3b7bc512aca8477cc1f792f7cf260f073
miranda0111 <75111602+miranda0111@users.noreply.github.com>
2022-08-18 12:51:03 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,8 +1,7 @@
 const MMSLS = [
-    
-    "https://www.maimemo.com/share/page?uid=15249640&pid=d9854f92d3dbb94c2b6ac1c01be49876&tid=73efc8782196dd1bb0c9300ec4c2cf44",
-    "https://www.maimemo.com/share/page?tid=90a7e415965bfe5de96a042d8331bcd3&uid=17134313&pid=d9854f92d3dbb94c2b6ac1c01be49876", 
-    "https://www.maimemo.com/share/page?tid=8876329e71f844975846db7ce05c0374&uid=21955403&pid=d9854f92d3dbb94c2b6ac1c01be49876",
+ 
+"https://www.maimemo.com/share/page?tid=90a7e415965bfe5de96a042d8331bcd3&uid=17134313&pid=d9854f92d3dbb94c2b6ac1c01be49876", 
+"https://www.maimemo.com/share/page?tid=8876329e71f844975846db7ce05c0374&uid=21955403&pid=d9854f92d3dbb94c2b6ac1c01be49876",
 ]
   /**
    * 生成随机数字
